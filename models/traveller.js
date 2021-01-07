cbe0f541c111ae12b4f95c000032165fa715fc89
Alex Dodd <alexdodd@gmail.com>
2021-01-07 22:59:45 +0000

    test 4 passed

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,15 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  return this.journeys.map(journey => journey.startLocation)
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  return this.journeys.map(journey => journey.endLocation)
+  return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
